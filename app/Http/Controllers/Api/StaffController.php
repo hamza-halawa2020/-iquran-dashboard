9fb6578466b1ee0a4831889ca44edac1568b00b6
hamza-halawa2020 <h.halawa2020@gmail.com>
2026-05-18 15:43:32 +0300

    update

--- app/Http/Controllers/Api/StaffController.php
+++ app/Http/Controllers/Api/StaffController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Resources\Api\StaffResource;
 use App\Models\Staff;
+use Illuminate\Http\Request;
 
 class StaffController extends ApiController
 {
@@ -11,5 +12,28 @@ class StaffController extends ApiController
     {
         $this->model = Staff::class;
         $this->resource = StaffResource::class;
+    }
+
+    public function index(Request $request)
+    {
+        $query = $this->model::with($this->with)->orderBy('id', 'asc');
+
+        if (method_exists(new $this->model, 'scopeActive')) {
+            $query->active();
+        }
+
+        foreach ($this->filterableFields as $field) {
+            if ($request->has($field) && $request->get($field) !== null && $request->get($field) !== 'all') {
+                $query->where($field, $request->get($field));
+            }
+        }
+
+        if ($this->paginate) {
+            $items = $query->paginate($request->get('limit', $this->perPage));
+        } else {
+            $items = $query->get();
+        }
+
+        return $this->resource::collection($items);
     }
 }
